d83c31559602b0a29f5012577fe566e71055de04
JacekKorta <jacek.korta@gmail.com>
2022-09-24 19:18:09 +0200

    Add basic configuration

---
+++ src/config.py
@@ -0,0 +1,38 @@
+from functools import lru_cache
+from pydantic import BaseSettings, Field
+
+
+class RedisSettings(BaseSettings):
+    host: str = Field("localhost", env="REDIS_HOST")
+    port: int = Field(6379, env="REDIS_PORT")
+    key_ttl: int = Field(3600, env="REDIS_KEY_TTL")
+
+    class Config:
+        env_file = ".env"
+
+
+class RabbitMQSettings(BaseSettings):
+    user: str = Field("guest", env="RABBITMQ_USER")
+    password: str = Field("guest", env="RABBITMQ_PASSWORD")
+    host: str = Field("localhost", env="RABBITMQ_HOST")
+    port: int = Field(5672, env="RABBITMQ_PORT")
+    vhost: str = Field(..., env="RABBITMQ_VHOST")
+    input_queue_name: str = Field(..., env="RABBITMQ_INPUT_QUEUE")
+    output_exchange_name: str = Field(..., env="RABBITMQ_OUTPUT_EXCHANGE")
+    output_routing_key: str = Field(..., env="RABBITMQ_OUTPUT_ROUTING_KEY")
+
+    class Config:
+        env_file = ".env"
+
+
+class Settings(BaseSettings):
+    rabbit: RabbitMQSettings = RabbitMQSettings()
+    redis: RedisSettings = RedisSettings()
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache()
+def get_settings() -> Settings:
+    return Settings()
